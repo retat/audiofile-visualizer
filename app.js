feb506498411259b33a43e29e3419bd6bb590d2b
René Borner <mail@reneborner.de>
2019-09-18 15:31:37 -0400

    refactoring and triangle design changes

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 let stopped = true
 const start = function () {
-    if (stopped) {
-        stopped = false
+    function createElements() {
         document.getElementById("container").style.height = "100px"
         document.getElementById("toggle").innerText = "Pause"
         const canvas = document.createElement('canvas')
         canvas.id = "mycanvas"
         document.body.appendChild(canvas)
+        const audioElement = document.createElement('audio')
+        audioElement.id = "myaudio"
+        document.body.appendChild(audioElement)
+        return {canvas, audioElement}
+    }
+
+    function setupCanvas(canvas) {
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
         const ctx = canvas.getContext('2d')
         const audioCtx = new AudioContext()
-        const audioElement = document.createElement('audio')
-        audioElement.id = "myaudio"
-        document.body.appendChild(audioElement)
         const analyser = audioCtx.createAnalyser()
         let power = 1
         while (power < window.innerWidth * 2)
             power <<= 1
         analyser.fftSize = power
+        return {ctx, audioCtx, analyser}
+    }
+
+    function createAudioVisualizer() {
+        stopped = false
+        const {canvas, audioElement} = createElements()
+        const {ctx, audioCtx, analyser} = setupCanvas(canvas)
         const player = audioCtx.createMediaElementSource(audioElement)
         player.connect(audioCtx.destination)
         player.connect(analyser)
@@ -29,43 +39,59 @@ const start = function () {
         let forwards = true
         draw = function () {
             window.requestAnimationFrame(draw)
-            let grd = ctx.createLinearGradient(0, 0, canvas.width, 0)
-            ctx.fillStyle = 'white'
-            ctx.fillRect(0, 0, canvas.width, canvas.height)
-            grd.addColorStop(0, 'red')
-            grd.addColorStop(1 / 6, 'orange')
-            grd.addColorStop(2 / 6, 'yellow')
-            grd.addColorStop(3 / 6, 'green')
-            grd.addColorStop(4 / 6, 'aqua')
-            grd.addColorStop(5 / 6, 'blue')
-            grd.addColorStop(1, 'purple')
-            ctx.fillStyle = grd
+
+            function createGradient() {
+                let grd = ctx.createLinearGradient(0, 0, canvas.width, 0)
+                ctx.fillStyle = 'white'
+                ctx.fillRect(0, 0, canvas.width, canvas.height)
+                grd.addColorStop(0, 'red')
+                grd.addColorStop(1 / 6, 'orange')
+                grd.addColorStop(2 / 6, 'yellow')
+                grd.addColorStop(3 / 6, 'green')
+                grd.addColorStop(4 / 6, 'aqua')
+                grd.addColorStop(5 / 6, 'blue')
+                grd.addColorStop(1, 'purple')
+                ctx.fillStyle = grd
+            }
+
+            createGradient()
             analyser.getByteFrequencyData(results)
             for (let i = 0; i < analyser.frequencyBinCount; i++) {
                 ctx.fillRect(i, canvas.height, 1, results[i] * 3 * -1) // upside down
             }
-            if (x === canvas.width - 50)
-                forwards = false
-            if (x === 0)
-                forwards = true
-            if (forwards) {
-                ctx.beginPath()
-                ctx.moveTo(50+x, 50)
-                ctx.lineTo(50+x, 125)
-                ctx.lineTo(150+x, 125)
-                x++
-                ctx.closePath()
-                ctx.fill()
-            } else {
-                x -= 1
-                ctx.moveTo(50+x, 50)
-                ctx.lineTo(50+x, 125)
-                ctx.lineTo(150+x, 125)
-                ctx.closePath()
-                ctx.fill()
+
+            function createTriangle(y) {
+                if (x >= canvas.width)
+                    forwards = false
+                if (x <= 0)
+                    forwards = true
+                if (forwards) {
+                    ctx.beginPath()
+                    ctx.moveTo(50 + x * 1.5, y)
+                    ctx.lineTo((50 + x) * 1.5, y * 2.5)
+                    ctx.lineTo(150 + x, y * 2.5 * -1.5)
+                    x += 5
+                    ctx.closePath()
+                    ctx.fill()
+                } else {
+                    x -= 5
+                    ctx.moveTo(50 + x * 1.5, y)
+                    ctx.lineTo((50 + x) * 1.5, y * 2.5)
+                    ctx.lineTo(150 + x, y * 2.5 * -1.5)
+                    ctx.closePath()
+                    ctx.fill()
+                }
             }
+
+            createTriangle(50)
+            //createTriangle(250)
+
         }
         draw()
+    }
+
+    if (stopped) {
+        createAudioVisualizer()
     } else {
         document.getElementById("container").style.height = "100vh"
         document.getElementById("toggle").innerText = "Play"
